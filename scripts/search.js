56c31c16a35fc25772ccaed2cdeb1a2fed2291dd
Florescu Cristian <qflorescucristian@gmail.com>
2023-06-23 18:55:02 +0300

    Added function to read products from list and display them.

--- scripts/search.js
+++ scripts/search.js
@@ -4,14 +4,25 @@ function search() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("product-list");
     li = ul.getElementsByTagName('article');
+    nomatches = document.getElementById("no-matches");
   
+    
+    var count = 0;
     for (i = 0; i < li.length; i++) {
       p = li[i].getElementsByTagName("div")[0];
       txtValue = p.textContent || p.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
         li[i].style.display = "";
+        count = count + 1;
       } else {
         li[i].style.display = "none";
       }
+    }
+    if(count == 0){
+      ul.classList.add('change-product-list');
+      nomatches.classList.add('change-no-matches');
+    } else{
+      ul.classList.remove('change-product-list');
+      nomatches.classList.remove('change-no-matches');
     }
 }
