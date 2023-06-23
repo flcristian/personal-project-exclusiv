cec416114e82cd4dee5d1f05e0e77ed8db06d789
Florescu Cristian <qflorescucristian@gmail.com>
2023-06-23 23:28:07 +0300

    Added filters and modified search function.

--- scripts/search.js
+++ scripts/search.js
@@ -1,28 +1,178 @@
-function search() {
-    var input, filter, ul, li, a, i, txtValue;
+function createProduct(data){
+  // ADDING PRODUCT ITEM ARTICLE
+  var productItem = document.createElement("article");
+  productItem.classList.add("product-item");
+
+  // ADDING PRODUCT IMAGE
+  var productImage = document.createElement("img");
+  productImage.classList.add("product-image");
+  productImage.src = data.path;
+  productImage.alt = '';
+
+  // ADDING PRODUCT DESCRIPTION
+  var productDescription = document.createElement("div");
+  productDescription.classList.add('product-description');
+  productDescription.textContent = data.description;
+
+  // ADDING PRODUCT PRICE
+  var productPrice = document.createElement("div");
+  productPrice.classList.add("product-price");
+  productPrice.textContent = data.price + " RON";
+
+  // ADDING PRODUCT BUTTON
+  var productButtonArea = document.createElement("aside");
+  productButtonArea.classList.add("product-button");
+  var productButton = document.createElement("a");
+  productButton.classList.add("product-button-text");
+  productButton.href = "contact.html";
+  productButton.textContent = "Contact";
+  productButtonArea.appendChild(productButton);
+
+  // ADDING PROPERTIES TO PRODUCT ITEM
+  productItem.appendChild(productImage);
+  productItem.appendChild(productDescription);
+  productItem.appendChild(productPrice);
+  productItem.appendChild(productButtonArea);
+
+  return productItem;
+}
+
+function handleKeyPress(event) {
+  if(event.key == "Enter"){
+    displayWithFilters();
+  }
+}
+
+function returnData(){
+  return fetch('data/products.json')
+    .then(response => response.json())
+    .then(data => {
+      return data;
+    });
+}
+
+function filterDataPriceAscending(data){
+  data.sort(function(a, b){
+    return a.price - b.price;
+  });
+}
+
+function filterDataPriceDescending(data){
+  data.sort(function(a, b){
+    return b.price - a.price;
+  });
+}
+
+function applyFilters(data){
+  var filter = document.getElementById("products-filters");
+
+  switch(filter.value){
+    case "none":
+      break;
+    case "price-ascending":
+      filterDataPriceAscending(data);
+      break;
+    case "price-descending":
+      filterDataPriceDescending(data);
+      break;
+  }
+}
+
+function displayWithFilters(){
+  returnData()
+  .then(data =>{
+    applyFilters(data);
+    searchtest(data);
+  });
+}
+
+function containsInDescription(description, filter){
+  if(description.toUpperCase().indexOf(filter) > -1){
+    return true;
+  }
+  return false;
+}
+
+function containsInTags(tags, tagcount, filter){
+  for(i = 0;i<tagcount;i++){
+    if(tags[i].toUpperCase().indexOf(filter) > -1){
+      return true;
+    }
+  }
+  return false;
+}
+
+function search(){
+    var input, filter, list, product, nomatches, i;
     input = document.getElementById('search-input');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("product-list");
-    li = ul.getElementsByTagName('article');
+    list = document.getElementById("product-list");
     nomatches = document.getElementById("no-matches");
-  
-    
-    var count = 0;
-    for (i = 0; i < li.length; i++) {
-      p = li[i].getElementsByTagName("div")[0];
-      txtValue = p.textContent || p.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        li[i].style.display = "";
-        count = count + 1;
-      } else {
-        li[i].style.display = "none";
+
+    while(list.firstChild){
+      list.removeChild(list.firstChild);
+    }
+
+    return fetch('data/products.json')
+    .then(response => response.json())
+    .then(data => {
+      var count = 0;
+      for(i = 0;i<data.length;i++){
+        var tagcount = 0;
+        while(data[i].tags[tagcount]){
+          tagcount++;
+        }
+
+        var products = [];
+        if(containsInDescription(data[i].description, filter) || containsInTags(data[i].tags, tagcount, filter)){
+          list.appendChild(createProduct(data[i]));
+          count = count + 1;
+        }
       }
+        
+      if(count == 0){
+         list.classList.add('change-product-list');
+        nomatches.classList.add('change-no-matches');
+      } 
+      else{
+         list.classList.remove('change-product-list');
+         nomatches.classList.remove('change-no-matches');
+      }
+      return data;
+    });
+}
+
+function searchtest(data){
+  var input, filter, list, product, nomatches, i;
+  input = document.getElementById('search-input');
+  filter = input.value.toUpperCase();
+  list = document.getElementById("product-list");
+  nomatches = document.getElementById("no-matches");
+
+  while(list.firstChild){
+    list.removeChild(list.firstChild);
+  }
+
+  var count = 0;
+  for(i = 0;i<data.length;i++){
+    var tagcount = 0;
+    while(data[i].tags[tagcount]){
+      tagcount++;
     }
-    if(count == 0){
-      ul.classList.add('change-product-list');
-      nomatches.classList.add('change-no-matches');
-    } else{
-      ul.classList.remove('change-product-list');
-      nomatches.classList.remove('change-no-matches');
+
+    var products = [];
+    if(containsInDescription(data[i].description, filter) || containsInTags(data[i].tags, tagcount, filter)){
+      list.appendChild(createProduct(data[i]));
+      count = count + 1;
     }
+  }
+    
+  if(count == 0){
+     list.classList.add('change-product-list');
+    nomatches.classList.add('change-no-matches');
+  } 
+  else{
+     list.classList.remove('change-product-list');
+     nomatches.classList.remove('change-no-matches');
+  }
 }
